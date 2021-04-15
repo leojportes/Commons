1e08e24a49455efb4f355544226ef6af6b77868c
leojportes <leojportes@gmail.com>
2021-04-14 21:32:49 -0300

    Initial commit

---
+++ Example/Commons/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Commons
+//
+//  Created by leojportes on 04/14/2021.
+//  Copyright (c) 2021 leojportes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
