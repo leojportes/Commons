7b4783578362c31fbd56baa19c155c89cae8b19d
leojportes <leojportes@gmail.com>
2021-04-20 21:08:58 -0300

    Alteraçao Link API

--- Commons/Classes/ApiRest.swift
+++ Commons/Classes/ApiRest.swift
@@ -11,7 +11,6 @@ import Foundation
 public struct ApiRest {
 
     public static let TodasAsMoedas = "https://rest.coinapi.io/v1/assets?apikey=1F8A5E86-F1C9-41C7-B8BB-9DB1B81FDE7C"
-    public static let MoedaDetalhe = "https://rest-sandbox.coinapi.io/v1/assets/@@@?apikey=1F8A5E86-F1C9-41C7-B8BB-9DB1B81FDE7C"
+    public static let MoedaDetalhe = "https://rest.coinapi.io/v1/assets/@@@?apikey=1F8A5E86-F1C9-41C7-B8BB-9DB1B81FDE7C"
     public static let UrlIcon = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_32/@@@.png"
 }
-
